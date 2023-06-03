de65a124924cac0081d98eae85cac5b90eb6aad0
unknown <you@example.com>
2023-06-04 01:26:30 +0600

    user collection added

--- index.js
+++ index.js
@@ -37,8 +37,20 @@ async function run() {
     const cartCollection = client.db("zaraDB").collection("carts");
 
     // users related apis
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res)=>{
       const user = req.body;
+      console.log(user);
+      const query = {email:user.email}
+      const existingUser = await usersCollection.findOne(query);
+      // console.log('existing user', existingUser)
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result)
     })
