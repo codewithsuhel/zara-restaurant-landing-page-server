12b1023cebea3daa9f817586d328084a0cc0938f
unknown <you@example.com>
2023-06-05 01:29:50 +0600

    awt token create

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+// jwt token create command:- node, require('crypto').randomBytes(64).toString('hex')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -40,7 +42,7 @@ async function run() {
     app.get('/users', async(req, res)=>{
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     app.post('/users', async(req, res)=>{
       const user = req.body;
@@ -52,6 +54,19 @@ async function run() {
         return res.send({message: 'user already exists'})
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result)
+    });
+
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const filter = { _id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role:'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
 
